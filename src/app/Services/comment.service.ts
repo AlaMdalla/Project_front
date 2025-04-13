2b83f761e06924b45c31a446a186a963a86e9908
elyesbejaoui2001 <elyes.bejaoui@esprit.tn>
2025-04-13 18:08:06 +0100

    reactcommentaire

--- src/app/Services/comment.service.ts
+++ src/app/Services/comment.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Urls } from '../config/Urls';
 
@@ -12,38 +12,33 @@ export class CommentService {
   constructor(private http: HttpClient) {}
 
   createComment(userId: number, postId: number, content: string): Observable<any> {
-    const body = {
-        userId: userId,
-        postId: postId,
-        content: content
-    };
-    return this.http.post(`${BASIC_URL}blog/comments/create`, body);
-}
+    const headers = new HttpHeaders({ 'User-Id': userId.toString() });
+    const params = new HttpParams()
+      .set('postId', postId.toString())
+      .set('content', content);
+
+    console.log("Sending comment: { userId:", userId, ", postId:", postId, ", content:", content, "}");
+
+    return this.http.post(`${BASIC_URL}blog/comments/create`, null, { headers, params });
+  }
 
   getAllCommentByPost(postId: number): Observable<any> {
     return this.http.get(`${BASIC_URL}blog/comments/post/${postId}`);
   }
 
-  replyToComment(userId: number, commentId: number, content: string): Observable<any> {
-    const body = {
-        userId: userId,
-        parentCommentId: commentId,
-        content: content
-    };
-    return this.http.post(`${BASIC_URL}blog/comments/create`, body);
-}
+  replyToComment(userId: number, parentCommentId: number, content: string): Observable<any> {
+    const params = new HttpParams()
+      .set('parentCommentId', parentCommentId.toString())
+      .set('userId', userId.toString())
+      .set('content', content);
+
+    return this.http.post(`${BASIC_URL}blog/comments/reply`, null, { params });
+  }
 
   reactComment(userId: number, commentId: number, reaction: string): Observable<any> {
-    const params = {
-      reaction: reaction
-    };
-    return this.http.put(
-      `${BASIC_URL}blog/comments/${commentId}/react`,
-      null,
-      {
-        params,
-        headers: { 'User-Id': userId.toString() }
-      }
-    );
+    const params = { reaction };
+    const headers = new HttpHeaders({ 'User-Id': userId.toString() });
+
+    return this.http.put(`${BASIC_URL}blog/comments/${commentId}/react`, null, { headers, params });
   }
 }
